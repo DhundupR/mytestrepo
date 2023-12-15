ed40b11d550af0a8303ae092569bc32708a337a6
BT_1N3_27 <bths@sample>
2023-12-15 09:07:52 -0500

    3rd commit

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,11 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        int l = 0;
        System.out.println("UNO");
        for(int i=0;i<100;i++) {
            System.out.println("You're fired");
        }
+       while(l < 1){ System.out.println("L");}
     }
 }
