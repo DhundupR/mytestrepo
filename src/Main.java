fcec79a4d0a44f099898ce37973399a8296c1593
BT_1N3_28 <bths.edu@sample>
2023-12-15 09:06:04 -0500

    commit test 2

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 public class Main {
     public static void main(String[] args) {
        System.out.println("UNO");
-       for(int i;i<100;i++) {
+       for(int i=0;i<100;i++) {
            System.out.println("You're fired");
        }
     }
